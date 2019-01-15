11447afee7777490325b80a8fe8e15cbb60248d0
Sam <sam.d.odonnell@gmail.com>
2019-01-14 21:01:31 -0800

    Add semi-completed homework

--- W2D2/lib/board.rb
+++ W2D2/lib/board.rb
@@ -2,16 +2,35 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @player1 = name1
+    @player2 = name2
+    @cups = Array.new(14) {[]}
+    place_stones
+  end
+
+  def []=(pos, val)
+    x,y = pos
+    [x][y]
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    self.cups.each_with_index do |cup, i| 
+      4.times {cup << :stone} unless i == 6 || i == 13
+    end
   end
 
   def valid_move?(start_pos)
+    if !(0..5).include?(start_pos) && !(7..12).include?(start_pos)
+      raise "Invalid starting cup"
+    elsif self.cups[start_pos].empty?
+      raise "Starting cup is empty"
+    end
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    start_cup = self.cups[start_pos]
+    cups[start_pos..start_pos+start_cup.length]
   end
 
   def next_turn(ending_cup_idx)
